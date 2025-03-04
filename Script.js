c8603a5235e7ec19b59f25c72dfc22649308918f
Jeffrey Butela <j.butela@yahoo.com>
2025-03-03 22:06:46 -0500

    First Commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+
+// setting up the message that will appear
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+// defining the sum of the variables and assigning them to a tag as a number
+const num1 = 2 * 5 // 10
+const num2 = 42 - 2 // 40
+const num3 = 38 + 1 // 39
+
+// concatinating the string message with the numbers and dashes
+alert(`${message}${num1}-${num2}-${num3}`);
+
+
+
+
+
